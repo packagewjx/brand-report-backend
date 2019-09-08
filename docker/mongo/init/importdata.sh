cb509445d41621b89a1f0fe07a732cd8e13e97fe
Wujunxian <wu812730157@gmail.com>
2019-09-08 14:50:15 +0800

    删除无用的mongoinit文件，修改测试日志层级

--- docker/mongo/init/importdata.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mongoimport --db brand_report --collection brand --file /docker-entrypoint-initdb.d/data/brand.json
-mongoimport --db brand_report --collection collection --file /docker-entrypoint-initdb.d/data/collection.json
-mongoimport --db brand_report --collection index --file /docker-entrypoint-initdb.d/data/index.json
