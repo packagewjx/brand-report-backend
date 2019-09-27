ddaef8be6c6dc206c43fca841b01c5e978396913
Francis-Zheng <824254922@qq.com>
2019-09-27 21:43:43 +0800

    修复docker镜像配置的一些问题

--- docker/beforestart.sh
+++ docker/beforestart.sh
@@ -5,6 +5,6 @@ cd ../
 
 # 构建jar文件
 echo "构建后台程序jar文件"
-mvn package spring-boot:repackage
+mvn package -DskipTests spring-boot:repackage
 cp target/brand-report-backend-0.0.1-SNAPSHOT.jar docker/backend/
 cp -r data/ docker/backend/
